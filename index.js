32b5730e6399e8c023fcaa0cd313f015c16629bd
AdniSosa <adni.sori@gmail.com>
2025-04-09 23:31:47 +0200

    Cambios por la autenticación

--- index.js
+++ index.js
@@ -5,11 +5,18 @@ const admin = require('firebase-admin');
 const serviceAccount = require('./config/firebase.js');
 const cookieParser = require('cookie-parser');
 const authRoutes = require('./routes/authRoutes');
-const routes = require('./routes/services');
+const servicesRoutes = require('./routes/servicesRoutes.js');
 require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 const cors = require('cors');
+
+const corsOptions = {
+  origin: 'http://localhost:5173', // El origen de tu app en React
+  credentials: true, // 🔥 Importante para que las cookies funcionen
+};
+
+app.use(cors(corsOptions));
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -21,7 +28,7 @@ app.use(cookieParser());
 
 dbConnection()//conexión bbdd mongoo
 app.use('/', authRoutes);
-app.use('/', routes); 
+app.use('/', servicesRoutes); 
 
 app.listen(PORT, () => {
     console.log(`Express está escuchando en el puerto http://localhost:${PORT}`)
---
+++ middlewares/verifyToken.js
@@ -0,0 +1,22 @@
+const admin = require('firebase-admin');
+
+const verifyToken = async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res.status(401).json({ message: "Token no enviado" });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    try {
+        const decodedToken = await admin.auth().verifyIdToken(token);
+        req.user = decodedToken;
+        next();
+    } catch (error) {
+        console.error("Token inválido:", error.message);
+        res.status(401).json({ message: "Token inválido" });
+    }
+};
+
+module.exports = verifyToken;
