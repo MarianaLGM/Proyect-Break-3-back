3cebccc59917aec0b7a326e4acf996fbee7b7ce2
MarianaLGM <mariana_lobeto@hotmail.com>
2025-04-23 12:57:22 +0200

    cambios

--- routes/servicesRoutes.js
+++ routes/servicesRoutes.js
@@ -3,13 +3,16 @@ const router = express.Router()
 const {ServicesControllers, AppointmentsControllers} = require('../controllers/ServicesControllers');
 const verifyToken = require("../middlewares/verifyToken");
 
-router.post("/create", verifyToken, ServicesControllers.create)
+router.post('/create', verifyToken, ServicesControllers.create)
 router.get('/', ServicesControllers.getAll )
-router.get('/id/:_id', ServicesControllers.getByID)
+router.get('/id/:_id', verifyToken, ServicesControllers.getByID)
 router.put('/id/:_id', verifyToken, ServicesControllers.updateById) 
 router.delete('/id/:_id', verifyToken, ServicesControllers.deleteService )
 
+router.get('/tratamientos-faciales/mas-info/id/:_id', ServicesControllers.getByTratamientoFacialId)
 router.get('/tratamientos-faciales', ServicesControllers.getByTratamientoFacial)
+
+router.get('/tratamientos-corporales/mas-info/id/:_id',ServicesControllers.getByTratamientoCorporalId)
 router.get('/tratamientos-corporales',ServicesControllers.getByTratamientoCorporal)
 
 router.post("/appointment", AppointmentsControllers.create)
