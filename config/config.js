15c0a95656a60901515da2beff35b5b03991410f
AdniSosa <adni.sori@gmail.com>
2025-04-25 10:31:18 +0200

    Cambios para hacer deploy

--- config/config.js
+++ config/config.js
@@ -5,7 +5,6 @@ require('dotenv').config()
 const dbConnection = async() => {
     try {
     await mongoose.connect(process.env.MONGO_URI)
-    console.log(process.env.MONGO_URI)
     console.log('Base de datos conectada con éxito')
     } catch (error) {
     console.log(error)
